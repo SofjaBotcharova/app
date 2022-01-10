534d2bae763de8b1c1f636d07455d4e02adda5c1
SofjaBotcharova <84132430+SofjaBotcharova@users.noreply.github.com>
2022-01-10 09:16:57 +0200

    Update errors_app_v6.r

--- errors_app_v6.r
+++ errors_app_v6.r
@@ -3,9 +3,9 @@
 # Find out more about building applications with Shiny here:
 # http://shiny.rstudio.com/
 
-# очищаем кэш
+# Г®Г·ГЁГ№Г ГҐГ¬ ГЄГЅГё
 rm(list=ls())
-# очищаем кэш графиков
+# Г®Г·ГЁГ№Г ГҐГ¬ ГЄГЅГё ГЈГ°Г ГґГЁГЄГ®Гў
 graphics.off()
 
 library(shiny)
@@ -34,30 +34,21 @@ ui <- fluidPage(
   
   # Application title
   titlePanel("Temperature and humidity monitoring system"),
-  fluidPage(# Задаем, что элементы интерфейса начинаются с нового блока
+  fluidPage(
     
-    tabsetPanel(# Задаем первую закладку
-      tabPanel("Monitoring system setups", # имя первой закладки
+    tabsetPanel(
+      tabPanel("Monitoring system setups", # ГЁГ¬Гї ГЇГҐГ°ГўГ®Г© Г§Г ГЄГ«Г Г¤ГЄГЁ
                fluidRow(
-                 column(4, # Задаем, что элементы интерфейса ниже занимают 4/12 от ширину страницы
-                        # Задаем выбор лаборатории 
-                        selectInput("lab", "Laboratory name", choices = unique(loger_data$labor)),
-                        # Задаем выбор комнаты 
-                        selectInput("room", "Room number", choices = NULL),
-                        # Задаем выбор датчика температуры 
-                        selectInput("device_t", "Temperature sensor", choices = NULL),
-                        # Задаем выбор датчика влажности 
+                 column(4,  
+                        selectInput("lab", "Laboratory name", choices = unique(loger_data$labor)), 
+                        selectInput("room", "Room number", choices = NULL), 
+                        selectInput("device_t", "Temperature sensor", choices = NULL), 
                         selectInput("device_h", "Humidity sensor", choices = NULL),
-                        # Задаем текст 
                         p("Update data first, then update timeline"), 
                         p("Update data everytime you changed inputs"),
-                        # кнопка обновить 
                         actionButton("update", "Update!"),
-                        # Задаем текст 
                         p("Don't forget update timeline once"),
-                        # кнопка обновить временной лимир
                         actionButton("time_periond", "Update timeline once!"),   
-                        # бегунки временного лимита
                         sliderInput("time",
                                     "Time period (sec)",
                                     value = c(as.POSIXct("2020-01-01 00:00:07"),
@@ -65,44 +56,34 @@ ui <- fluidPage(
                                     min = as.POSIXct("2020-01-01 00:00:07"),
                                     max = as.POSIXct("2021-01-01 00:00:07"),
                                     timeFormat = "%Y-%m-%d %H:%M:%S"),
-                        # бегунки лимитов по температуре
                         sliderInput("t_limit", "Temperature limit (C)", value = c(20, 60), min = -50, max = 100),
-                        # бегунки лимитов по влажности
                         sliderInput("h_limit", "Humidity limit (%)", value = c(20, 80), min = 0, max = 100),
-                        # бегунки возможной температуры
                         sliderInput("t_poss", "Possible temperature (C)", value = c(-10, 100), min = -50, max = 200),
-                        # температура поверхности
                         numericInput("rosa", "Surface temperature (C)", value = 18, min = -50, max = 200),
-                        # временной лимит разрыва времени
                         # numericInput("sec_diff_time", "Time limit between signals (sec)", value = 306, min = 0),
-                        # текст
                         p("Push Plot once, after updating timeline"),
-                        # кнопка выведения графиков
                         actionButton("plot_", "Plot!"),
                         
                         textOutput("try")
                         
                  ),
-                 column(8, #  графики занимают 8/12 от толщинв страницы
-                        # график температуры за последний день
+                 column(8, 
                         dygraphOutput("dygraph_t"),
-                        # график влжности за последний день
                         dygraphOutput("dygraph_h"),
-                        # график влажности, температуры и росы за последний день
                         dygraphOutput("dygraph_r")
                  )
                )
       ),
-      tabPanel("Plots for errors", # заголовок второй страницы
+      tabPanel("Plots for errors", 
                fluidRow(
-                 column(6, # половину страницы занимает график с ошибками по температуре
+                 column(6,
                         plotlyOutput("plot_t")
                  ),
-                 column(6, # половину страницы занимает график с ошибками по влажности
+                 column(6, 
                         plotlyOutput("plot_h")
                  )
                ),
-               fluidRow( # внизу под графиками вывод таблицы с выводами
+               fluidRow( 
                  column(6, # 
                         dataTableOutput("SUMMARY_out_T")
                  )
@@ -119,43 +100,43 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
-  # берем нижний лимит для температуры из бегунков в интерфейсе
+  # ГЎГҐГ°ГҐГ¬ Г­ГЁГ¦Г­ГЁГ© Г«ГЁГ¬ГЁГІ Г¤Г«Гї ГІГҐГ¬ГЇГҐГ°Г ГІГіГ°Г» ГЁГ§ ГЎГҐГЈГіГ­ГЄГ®Гў Гў ГЁГ­ГІГҐГ°ГґГҐГ©Г±ГҐ
   t_low_limit <- eventReactive(input$update, {
     min(input$t_limit)
   })
-  # берем верхний лимит для температуры из бегунков в интерфейсе
+  # ГЎГҐГ°ГҐГ¬ ГўГҐГ°ГµГ­ГЁГ© Г«ГЁГ¬ГЁГІ Г¤Г«Гї ГІГҐГ¬ГЇГҐГ°Г ГІГіГ°Г» ГЁГ§ ГЎГҐГЈГіГ­ГЄГ®Гў Гў ГЁГ­ГІГҐГ°ГґГҐГ©Г±ГҐ
   t_top_limit <- eventReactive(input$update, {
     max(input$t_limit)
   })
-  # берем нижний лимит для влажности из бегунков в интерфейсе
+  # ГЎГҐГ°ГҐГ¬ Г­ГЁГ¦Г­ГЁГ© Г«ГЁГ¬ГЁГІ Г¤Г«Гї ГўГ«Г Г¦Г­Г®Г±ГІГЁ ГЁГ§ ГЎГҐГЈГіГ­ГЄГ®Гў Гў ГЁГ­ГІГҐГ°ГґГҐГ©Г±ГҐ
   h_low_limit <- eventReactive(input$update, {
     min(input$h_limit)
   })
-  # берем верхний лимит для влажности из бегунков в интерфейсе
+  # ГЎГҐГ°ГҐГ¬ ГўГҐГ°ГµГ­ГЁГ© Г«ГЁГ¬ГЁГІ Г¤Г«Гї ГўГ«Г Г¦Г­Г®Г±ГІГЁ ГЁГ§ ГЎГҐГЈГіГ­ГЄГ®Гў Гў ГЁГ­ГІГҐГ°ГґГҐГ©Г±ГҐ
   h_top_limit <- eventReactive(input$update, {
     max(input$h_limit)
   })
-  # берем левый лимит для времени из бегунков в интерфейсе
+  # ГЎГҐГ°ГҐГ¬ Г«ГҐГўГ»Г© Г«ГЁГ¬ГЁГІ Г¤Г«Гї ГўГ°ГҐГ¬ГҐГ­ГЁ ГЁГ§ ГЎГҐГЈГіГ­ГЄГ®Гў Гў ГЁГ­ГІГҐГ°ГґГҐГ©Г±ГҐ
   time_limit_left <- eventReactive(input$update, {
     min(input$time)
   })
-  # берем правый лимит для времени из бегунков в интерфейсе
+  # ГЎГҐГ°ГҐГ¬ ГЇГ°Г ГўГ»Г© Г«ГЁГ¬ГЁГІ Г¤Г«Гї ГўГ°ГҐГ¬ГҐГ­ГЁ ГЁГ§ ГЎГҐГЈГіГ­ГЄГ®Гў Гў ГЁГ­ГІГҐГ°ГґГҐГ©Г±ГҐ
   time_limit_right <- eventReactive(input$update, {
     max(input$time)
   })
-  # берем нижний лимит для возможной температуры из бегунков в интерфейсе
+  # ГЎГҐГ°ГҐГ¬ Г­ГЁГ¦Г­ГЁГ© Г«ГЁГ¬ГЁГІ Г¤Г«Гї ГўГ®Г§Г¬Г®Г¦Г­Г®Г© ГІГҐГ¬ГЇГҐГ°Г ГІГіГ°Г» ГЁГ§ ГЎГҐГЈГіГ­ГЄГ®Гў Гў ГЁГ­ГІГҐГ°ГґГҐГ©Г±ГҐ
   t_low_filter <- eventReactive(input$update, {
     min(input$t_poss)
   })
-  # берем верхний лимит для возможной температуры из бегунков в интерфейсе
+  # ГЎГҐГ°ГҐГ¬ ГўГҐГ°ГµГ­ГЁГ© Г«ГЁГ¬ГЁГІ Г¤Г«Гї ГўГ®Г§Г¬Г®Г¦Г­Г®Г© ГІГҐГ¬ГЇГҐГ°Г ГІГіГ°Г» ГЁГ§ ГЎГҐГЈГіГ­ГЄГ®Гў Гў ГЁГ­ГІГҐГ°ГґГҐГ©Г±ГҐ
   t_top_filter <- eventReactive(input$update, {
     max(input$t_poss)
   })
-  # берем температуру поверхности для точки росы из интерфейса
+  # ГЎГҐГ°ГҐГ¬ ГІГҐГ¬ГЇГҐГ°Г ГІГіГ°Гі ГЇГ®ГўГҐГ°ГµГ­Г®Г±ГІГЁ Г¤Г«Гї ГІГ®Г·ГЄГЁ Г°Г®Г±Г» ГЁГ§ ГЁГ­ГІГҐГ°ГґГҐГ©Г±Г 
   t_rosa <- eventReactive(input$update, {
     max(input$rosa)
   })
-  # берем температуру поверхности для точки росы из интерфейса
+  # ГЎГҐГ°ГҐГ¬ ГІГҐГ¬ГЇГҐГ°Г ГІГіГ°Гі ГЇГ®ГўГҐГ°ГµГ­Г®Г±ГІГЁ Г¤Г«Гї ГІГ®Г·ГЄГЁ Г°Г®Г±Г» ГЁГ§ ГЁГ­ГІГҐГ°ГґГҐГ©Г±Г 
   diff_time <- eventReactive(input$update, {
     
     temp=read.csv(file = T_sensor(),header = T,sep = ";")
@@ -177,8 +158,8 @@ server <- function(input, output) {
   })
   
   
-  # активируем нашу ранее написаную функцию по расчету ошибок 
-  # когда нажимаем апдейт и ранее заданные парметры изинрефейса подгружаются в функцию
+  # Г ГЄГІГЁГўГЁГ°ГіГҐГ¬ Г­Г ГёГі Г°Г Г­ГҐГҐ Г­Г ГЇГЁГ±Г Г­ГіГѕ ГґГіГ­ГЄГ¶ГЁГѕ ГЇГ® Г°Г Г±Г·ГҐГІГі Г®ГёГЁГЎГ®ГЄ 
+  # ГЄГ®ГЈГ¤Г  Г­Г Г¦ГЁГ¬Г ГҐГ¬ Г ГЇГ¤ГҐГ©ГІ ГЁ Г°Г Г­ГҐГҐ Г§Г Г¤Г Г­Г­Г»ГҐ ГЇГ Г°Г¬ГҐГІГ°Г» ГЁГ§ГЁГ­Г°ГҐГґГҐГ©Г±Г  ГЇГ®Г¤ГЈГ°ГіГ¦Г ГѕГІГ±Гї Гў ГґГіГ­ГЄГ¶ГЁГѕ
   
   DATA <- eventReactive(input$update,{
     
@@ -197,9 +178,6 @@ server <- function(input, output) {
   
   
   
-  # данные для графика ошибок отвильтвовываются по заданному временному промежутку
-  # график рисуется после нажапия на плот
-  #  обнавляется после нажатия на апдейт
   DATA_plot <- reactive({
     input$plot_
     DATA_time <- DATA() %>%
@@ -225,8 +203,8 @@ server <- function(input, output) {
                all_error_for_humidity != "clear data",
              .preserve=T)
   })
-  # обновдление временного периода, так ка мы не занем какой файс с данными приходит
-  # и какое там время, оно подгрузится из файла при нажатии обновления временного лимита
+  # Г®ГЎГ­Г®ГўГ¤Г«ГҐГ­ГЁГҐ ГўГ°ГҐГ¬ГҐГ­Г­Г®ГЈГ® ГЇГҐГ°ГЁГ®Г¤Г , ГІГ ГЄ ГЄГ  Г¬Г» Г­ГҐ Г§Г Г­ГҐГ¬ ГЄГ ГЄГ®Г© ГґГ Г©Г± Г± Г¤Г Г­Г­Г»Г¬ГЁ ГЇГ°ГЁГµГ®Г¤ГЁГІ
+  # ГЁ ГЄГ ГЄГ®ГҐ ГІГ Г¬ ГўГ°ГҐГ¬Гї, Г®Г­Г® ГЇГ®Г¤ГЈГ°ГіГ§ГЁГІГ±Гї ГЁГ§ ГґГ Г©Г«Г  ГЇГ°ГЁ Г­Г Г¦Г ГІГЁГЁ Г®ГЎГ­Г®ГўГ«ГҐГ­ГЁГї ГўГ°ГҐГ¬ГҐГ­Г­Г®ГЈГ® Г«ГЁГ¬ГЁГІГ 
   observeEvent(input$time_periond,
                updateSliderInput(inputId = "time", 
                                  value = c(min(DATA()$time_t),
@@ -234,41 +212,41 @@ server <- function(input, output) {
                                  min = min(DATA()$time_t),
                                  max = max(DATA()$time_t))
   )
-  # выбор лабы
+  # ГўГ»ГЎГ®Г° Г«Г ГЎГ»
   Laboratory <- reactive({
     filter(loger_data, labor == input$lab)
   })
-  # выбор комнат в лабе
+  # ГўГ»ГЎГ®Г° ГЄГ®Г¬Г­Г ГІ Гў Г«Г ГЎГҐ
   observeEvent(Laboratory(), {
     choices <- unique(Laboratory()$ruum)
     updateSelectInput(inputId = "room", choices = choices) 
   })
-  # переменная для комнаты
+  # ГЇГҐГ°ГҐГ¬ГҐГ­Г­Г Гї Г¤Г«Гї ГЄГ®Г¬Г­Г ГІГ»
   Room <- reactive({
     req(input$room)
     filter(Laboratory(), ruum == input$room)
   })
-  # выбор комнаты
+  # ГўГ»ГЎГ®Г° ГЄГ®Г¬Г­Г ГІГ»
   observeEvent(Room(), {
     choices_t <- grep(glob2rx("T*"), unique(Room()$DEVICE_ID), value = TRUE)
     choices_h <- grep(glob2rx("H*"), unique(Room()$DEVICE_ID), value = TRUE)
     updateSelectInput(inputId = "device_t", choices = choices_t)
     updateSelectInput(inputId = "device_h", choices = choices_h)
   })
-  # переменная для датчика температуры 
+  # ГЇГҐГ°ГҐГ¬ГҐГ­Г­Г Гї Г¤Г«Гї Г¤Г ГІГ·ГЁГЄГ  ГІГҐГ¬ГЇГҐГ°Г ГІГіГ°Г» 
   T_sensor <- eventReactive(input$update,{
     req(input$device_t)
     t_sensor=str_sub(string = input$device_t, 3, nchar(input$device_t))
     paste(t_sensor,"csv",sep = ".")
   })
-  # переменная для датчика влажности
+  # ГЇГҐГ°ГҐГ¬ГҐГ­Г­Г Гї Г¤Г«Гї Г¤Г ГІГ·ГЁГЄГ  ГўГ«Г Г¦Г­Г®Г±ГІГЁ
   H_sensor <- eventReactive(input$update,{
     req(input$device_h)
     t_sensor=str_sub(string = input$device_t, 3, nchar(input$device_t))
     paste("Ruum_",t_sensor,"_-_H",".csv",sep = "")
   })
-  # отрисовка графика с ошибками для температуры при нажатиии плот
-  # и обновление при нажатии апдейт
+  # Г®ГІГ°ГЁГ±Г®ГўГЄГ  ГЈГ°Г ГґГЁГЄГ  Г± Г®ГёГЁГЎГЄГ Г¬ГЁ Г¤Г«Гї ГІГҐГ¬ГЇГҐГ°Г ГІГіГ°Г» ГЇГ°ГЁ Г­Г Г¦Г ГІГЁГЁГЁ ГЇГ«Г®ГІ
+  # ГЁ Г®ГЎГ­Г®ГўГ«ГҐГ­ГЁГҐ ГЇГ°ГЁ Г­Г Г¦Г ГІГЁГЁ Г ГЇГ¤ГҐГ©ГІ
   observeEvent(input$plot_,{
     output$plot_t <- renderPlotly({
       ggplot(DATA_plot_T(), aes(x = time_t, y = temperature)) +
@@ -279,8 +257,8 @@ server <- function(input, output) {
         labs(title = "Temperature and errors")
     })
   })
-  # отрисовка графика с ошибками для влажности при нажатиии плот
-  # и обновление при нажатии апдейт
+  # Г®ГІГ°ГЁГ±Г®ГўГЄГ  ГЈГ°Г ГґГЁГЄГ  Г± Г®ГёГЁГЎГЄГ Г¬ГЁ Г¤Г«Гї ГўГ«Г Г¦Г­Г®Г±ГІГЁ ГЇГ°ГЁ Г­Г Г¦Г ГІГЁГЁГЁ ГЇГ«Г®ГІ
+  # ГЁ Г®ГЎГ­Г®ГўГ«ГҐГ­ГЁГҐ ГЇГ°ГЁ Г­Г Г¦Г ГІГЁГЁ Г ГЇГ¤ГҐГ©ГІ
   observeEvent(input$plot_,{
     output$plot_h <- renderPlotly({
       base <- ggplot(DATA_plot_H(), aes(x = time_h, y = humidity)) +
@@ -293,7 +271,7 @@ server <- function(input, output) {
     })
   })
   
-  # переменная с таблицей для саммари
+  # ГЇГҐГ°ГҐГ¬ГҐГ­Г­Г Гї Г± ГІГ ГЎГ«ГЁГ¶ГҐГ© Г¤Г«Гї Г±Г Г¬Г¬Г Г°ГЁ
   DATA_SUMMARY <- reactive({
     input$plot_
     SUMMARY=data.frame(Temperature=as.factor(DATA_plot()$all_error_for_temperature),
@@ -312,8 +290,8 @@ server <- function(input, output) {
     SUMMARY=data.frame(Humidity=as.factor(DATA_plot_H()$all_error_for_humidity))
     summary(SUMMARY)
   })
-  # рендер таблицы при нажатиии на плот
-  # обновление при нажатии на апдейт
+  # Г°ГҐГ­Г¤ГҐГ° ГІГ ГЎГ«ГЁГ¶Г» ГЇГ°ГЁ Г­Г Г¦Г ГІГЁГЁГЁ Г­Г  ГЇГ«Г®ГІ
+  # Г®ГЎГ­Г®ГўГ«ГҐГ­ГЁГҐ ГЇГ°ГЁ Г­Г Г¦Г ГІГЁГЁ Г­Г  Г ГЇГ¤ГҐГ©ГІ
   # observeEvent(input$plot_,{
   #   output$SUMMARY_out <- renderDataTable(DATA_SUMMARY()
   #   )
@@ -326,7 +304,7 @@ server <- function(input, output) {
     output$SUMMARY_out_H <- renderDataTable(DATA_SUMMARY_H()
     )
   })
-  # данные для графика для температуры за последний день с лимитами
+  # Г¤Г Г­Г­Г»ГҐ Г¤Г«Гї ГЈГ°Г ГґГЁГЄГ  Г¤Г«Гї ГІГҐГ¬ГЇГҐГ°Г ГІГіГ°Г» Г§Г  ГЇГ®Г±Г«ГҐГ¤Г­ГЁГ© Г¤ГҐГ­Гј Г± Г«ГЁГ¬ГЁГІГ Г¬ГЁ
   DATA_dygraph_t <- reactive({
     input$plot_
     xts_t <- DATA() %>%
@@ -335,7 +313,7 @@ server <- function(input, output) {
     zoo_t <- zoo(x=xts_t$temperature, order.by=xts_t$time_t)
     zoo_t
   })
-  # график для температуры за последний день с лимитами
+  # ГЈГ°Г ГґГЁГЄ Г¤Г«Гї ГІГҐГ¬ГЇГҐГ°Г ГІГіГ°Г» Г§Г  ГЇГ®Г±Г«ГҐГ¤Г­ГЁГ© Г¤ГҐГ­Гј Г± Г«ГЁГ¬ГЁГІГ Г¬ГЁ
   observeEvent(input$plot_,{
     output$dygraph_t <- renderDygraph(
       
@@ -348,7 +326,7 @@ server <- function(input, output) {
                 strokePattern = "solid", color = "red")
     )
   })
-  # данные для графика для влажности за последний день с лимитами
+  # Г¤Г Г­Г­Г»ГҐ Г¤Г«Гї ГЈГ°Г ГґГЁГЄГ  Г¤Г«Гї ГўГ«Г Г¦Г­Г®Г±ГІГЁ Г§Г  ГЇГ®Г±Г«ГҐГ¤Г­ГЁГ© Г¤ГҐГ­Гј Г± Г«ГЁГ¬ГЁГІГ Г¬ГЁ
   DATA_dygraph_h <- reactive({
     input$plot_
     xts_h <- DATA() %>%
@@ -357,7 +335,7 @@ server <- function(input, output) {
     zoo_h <- zoo(x=xts_h$humidity, order.by=xts_h$time_t)
     zoo_h
   })
-  # график для влажности за последний день с лимитами
+  # ГЈГ°Г ГґГЁГЄ Г¤Г«Гї ГўГ«Г Г¦Г­Г®Г±ГІГЁ Г§Г  ГЇГ®Г±Г«ГҐГ¤Г­ГЁГ© Г¤ГҐГ­Гј Г± Г«ГЁГ¬ГЁГІГ Г¬ГЁ
   observeEvent(input$plot_,{
     output$dygraph_h <- renderDygraph(
       
@@ -370,7 +348,7 @@ server <- function(input, output) {
                 strokePattern = "solid", color = "red")
     )
   })
-  # данные для графика для температура, влажности и точки росы
+  # Г¤Г Г­Г­Г»ГҐ Г¤Г«Гї ГЈГ°Г ГґГЁГЄГ  Г¤Г«Гї ГІГҐГ¬ГЇГҐГ°Г ГІГіГ°Г , ГўГ«Г Г¦Г­Г®Г±ГІГЁ ГЁ ГІГ®Г·ГЄГЁ Г°Г®Г±Г»
   DATA_dygraph_r <- reactive({
     input$plot_
     xts <- DATA() %>%
@@ -385,7 +363,7 @@ server <- function(input, output) {
     
     xts_all
   })
-  #  график для температура, влажности и точки росы за последний день
+  #  ГЈГ°Г ГґГЁГЄ Г¤Г«Гї ГІГҐГ¬ГЇГҐГ°Г ГІГіГ°Г , ГўГ«Г Г¦Г­Г®Г±ГІГЁ ГЁ ГІГ®Г·ГЄГЁ Г°Г®Г±Г» Г§Г  ГЇГ®Г±Г«ГҐГ¤Г­ГЁГ© Г¤ГҐГ­Гј
   observeEvent(input$plot_,{
     output$dygraph_r <- renderDygraph(
       
